ecbf1eb461a53fb86837007c96403920fd481629
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-28 08:41:21 -0500

    Actualizacion 1.0

---
+++ Bingo.py
@@ -0,0 +1,14 @@
+import random 
+def entrada():
+    str(input("Ingrese su nombre: "))
+    int(input("Ingrese su número de cartilla: "))
+
+cartilla = []
+def ball_aleat(n):
+    for i in range(n):
+        cartilla.insert(i,random.randrange(1,81))
+    return cartilla
+
+entrada()
+aleatorios = ball_aleat(15)
+print("Su cartilla es: ",aleatorios)
