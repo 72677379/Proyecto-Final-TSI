73bc76c28b6a6210eb96a79ef2471a4cc2132601
David Huanachin <lo.kito123@live.com>
2019-05-29 08:38:14 -0500

    Nueva version

--- Pozo total.py
+++ Pozo total.py
@@ -8,5 +8,5 @@ else:
         a=int(input("Ingrese un numero menor o igual a 3 : "))
         if a<=3:
             pozo_total = a * 5 
-            print("el pozo a pagar es  : ",pozo_total)
+            print("el pozo a pagar es : ",pozo_total)
             break 
