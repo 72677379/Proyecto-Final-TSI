2579e58f2da667cd671babf3309c21ad1e0e4ec4
Paul Solano <paulsolano2000@gmail.com>
2019-06-04 08:39:38 -0500

    ayuda

--- bingo2.py
+++ bingo2.py
@@ -20,33 +20,8 @@ def entrada():
                     else:
                       L.append(x)
                       i+=1
-                    print(jugadores, "Su codigo de cartilla es: ",L)
+                    print("Los números que estan en su cartilla son: ",L)
                     break
-              break 
-
-    if cartillas <= 3:
-        monto_persona = (cartillas * 5)*jugadores
-        print("el monto total a pagar es  : ",monto_persona)
-    else:
-        while cartillas > 3:
-            a=int(input("Ingrese un numero menor o igual a 3 : "))
-            if a<=3:
-              monto_persona = (a * 5)
-              print("el monto tatal a pagar es : ",monto_persona)
-              
-              L=[random.randint(0, 15)]
-              i=1 
-              while i<15:
-                x=random.randint(0,15)
-                for j in range(0, len(L)):
-                    if L[j]==x:
-                      break
-                    else:
-                      L.append(x)
-                      i+=1
-                    print("Su codigo de cartilla es: ",L)
-                    break
-
 entrada()
 
 L=[random.randint(0, 15)]
