a90219e52baa479738cd9620c38ca5b7c5928525
Paul Solano <paulsolano2000@gmail.com>
2019-06-05 07:24:29 -0500

    actualiza

--- bingo2.py
+++ bingo2.py
@@ -3,35 +3,44 @@ def entrada():
     jugadores = int(input("¿Cuantos jugadores son? "))
     for x in range (1,jugadores+1):
       comprador = str(input("¿Cual es su nombre? "))
-      cartillas = int(input("¿Cúantos cartillas desea comprar? "))
+      cartillas = int(input("¿Cúantos cartillas desea comprar?"))
 
-      while cartillas > 3:
-            a=int(input("Ingrese un numero menor o igual a 3 : "))
+      if cartillas <= 3:
+        monto_persona = cartillas * 5 
+        print("el pozo a pagar es: ",monto_persona)
+
+        L=[random.randint(0, 15)]
+        i=1
+        while i<15:
+          x=random.randint(0,15)
+          for j in range(0, len(L)):
+            if L[j]==x:
+              break
+            else:
+              L.append(x)
+              i+=1
+        print("Estas son las bolillas obtenidas", L)
+
+      else:
+        while cartillas > 3:
+            a=int(input("Ingrese un numero menor o igual a 3: "))
             if a<=3:
-              monto_persona = (a * 5)*jugadores
-              print("el monto total a pagar es : ",monto_persona)
-              L=[random.randint(0, 15)]
-              i=1 
-              while i<15:
-                x=random.randint(0,15)
-                for j in range(0, len(L)):
-                    if L[j]==x:
-                      break
-                    else:
-                      L.append(x)
-                      i+=1
-                    print("Los números que estan en su cartilla son: ",L)
-                    break
+              monto_persona = a * 5 
+              print("el pozo a pagar es: ",monto_persona)
+              
+
+            L=[random.randint(0, 15)]
+            i=1
+            while i<15:
+              x=random.randint(0,15)
+              for j in range(0, len(L)):
+                if L[j]==x:
+                  break
+                else:
+                  L.append(x)
+                  i+=1
+            print("Estas son las bolillas obtenidas", L)
+            break 
+              
 entrada()
 
-L=[random.randint(0, 15)]
-i=1
-while i<15:
-  x=random.randint(0,15)
-  for j in range(0, len(L)):
-    if L[j]==x:
-      break
-  else:
-    L.append(x)
-    i+=1
-print("Estas son las bolillas obtenidas", L)
