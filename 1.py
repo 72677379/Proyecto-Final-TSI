4ec2e0e9d80f9f24db98aa67d018b44c0a3c2a80
LuisSalvador <50873596+Dextro0x@users.noreply.github.com>
2019-06-11 08:38:22 -0500

    Actualizado

--- 1.py
+++ 1.py
@@ -1,12 +1,11 @@
 import random
-L=[random.randint(0, 15)]
-i=1
-while i<15:
-  x=random.randint(0,15)
-  for j in range(0, len(L)):
-    if L[j]==x:
-      break
-  else:
-    L.append(x)
-    i+=1
-print(L)
+
+def bolila(): 
+  miLista = list(range(1,80))
+  random.shuffle(miLista)
+  
+  miLista.append(miLista[:1])
+  print(miLista)
+
+bolila()
+
--- 2.py
+++ 2.py
@@ -2,21 +2,28 @@ import random
 import sys
 import os
 import sys
-z=1
-bsjugar = []
-bjugadas = []
-for i in range(1,81):
-    bsjugar.append(i)
-while z == 1:
-    for n in range(1,16):
-        a=int(input("Si desea otra bolilla presione (1):"))
-        juego = random.choice(bsjugar)
-        bsjugar.remove(juego)
-        bjugadas.append(juego)
-        print("Bolilla:",juego)
-        print("Bolillas que han salido: ",bjugadas)
-        print()
-    break
+def obtenerbol():
+    z=1
+    bsjugar = []
+    bjugadas = []
+    for i in range(1,81):
+        bsjugar.append(i)
+    while z == 1:
+        for n in range(1,16):
+            a=int(input("Si desea otra bolilla presione (1):"))
+            if a == 1:
+                juego = random.choice(bsjugar)
+                bsjugar.remove(juego)
+                bjugadas.append(juego)
+                print("Bolilla:",juego)
+                print("Bolillas que han salido: ",bjugadas)
+                print()
+        break
+            
 
 
+            
+                
+
+obtenerbol()
 
