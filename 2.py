4698c7f2bda5ba7a83feb990171be2863c736b17
david huanachin <lo.kito123@live.com>
2019-06-11 08:07:16 -0500

    nuevo aleatorio

--- 2.py
+++ 2.py
@@ -1,14 +1,22 @@
-#import random
-L=[random.randint(0, 15)]
-i=1
-while i<15:
-    x=random.randint(0,15)
-    for j in range(0, len(L)):
-        if L[j]==x:
-            break
-        else:
-            L.append(x)
-            i+=1
-            
-        print("los números de su cartilla son:", L)
-              
+import random
+import sys
+import os
+import sys
+z=1
+bsjugar = []
+bjugadas = []
+for i in range(1,81):
+    bsjugar.append(i)
+while z == 1:
+    for n in range(1,16):
+        a=int(input("Si desea otra bolilla presione (1):"))
+        juego = random.choice(bsjugar)
+        bsjugar.remove(juego)
+        bjugadas.append(juego)
+        print("Bolilla:",juego)
+        print("Bolillas que han salido: ",bjugadas)
+        print()
+    break
+
+
+
