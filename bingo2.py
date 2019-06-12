59a05aa0496e81c49a38d31fc6459626d4715301
LuisSalvador <50873596+Dextro0x@users.noreply.github.com>
2019-06-12 08:45:32 -0500

    Actualizacion

--- bingo2.py
+++ bingo2.py
@@ -2,11 +2,12 @@ import random
 import sys
 import os
 import sys
+
 def entrada():
     jugadores = int(input("¿Cuantos jugadores son? "))
     suma =0
     for x in range (1,jugadores+1):
-      comprador = str(input("¿Cual es su nombre? "))
+      comprador = (input("¿Cual es su nombre? "))
       cartillas = int(input("¿Cúantos cartillas desea comprar? "))
       if cartillas <= 3:
         monto_persona = cartillas * 5
@@ -31,21 +32,40 @@ def entrada():
                 print("Los números de su cartilla son: ", miLista[:15])     #mostramos únicamente los 15 primeros
               break 	        
     print("el poso total es : ",suma)            
-entrada()
-z=1
-bsjugar = []
-bjugadas = []
-for i in range(1,81):
-  bsjugar.append(i)
-while z == 1:
-    for n in range(1,16):
-      a=int(input("Si desea una bolilla presione (1):"))
-      #b=int(input("Si desea reiniciar su juego presione (2):"))
-      #c=int(input("Si desea salir del juego presione (3):"))
-      juego = random.choice(bsjugar)
-      bsjugar.remove(juego)
-      bjugadas.append(juego)
-      print("Bolilla:",juego)
-      print("Bolillas que han salido: ",bjugadas)
-      print()
+
+
+def sacar_bolillas():
+  z=1
+  bsjugar = []
+  bjugadas = []
+  for i in range(1,81):
+    bsjugar.append(i)
+  while z == 1:
+      for n in range(1,16):
+        a=int(input("Presione (9) si desea obtener bolilla"))
+        #b=int(input("Si desea reiniciar su juego presione (2):"))
+        #c=int(input("Si desea salir del juego presione (3):"))
+        if(a == 9):
+          juego = random.choice(bsjugar)
+          bsjugar.remove(juego)
+          bjugadas.append(juego)
+          print("Bolilla:",juego)
+          print("Bolillas que han salido: ",bjugadas)
+          print()
+      break
+
+
+while True:
+  print("Ingrese (1) para obtener datos de los compradores")
+  print("Ingrese (2) si desea ver bolilla salida")
+  print("Ingrese (0) si desea salir")
+  m = int(input()) 
+  if (m == 1):
+    entrada()
+    
+
+  elif (m == 2):
+    sacar_bolillas()
+  elif(m == 0):
+    print("Hasta pronto, usted se ha rendido mascota imbecil pareces Paul")
     break
